b27ab9ec8196c0c601644e7a76b7288da302b158
Bianca Eilertsen <biancaeilertsen@gmail.com>
2018-09-20 16:18:46 +0200

    kms

--- weather/src/main/java/com/forecast/html.java
+++ weather/src/main/java/com/forecast/html.java
@@ -1,11 +1,15 @@
 package com.forecast;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
+@RequestMapping("/weather.html")
 public class html  extends HttpServlet {
 
     @Override
